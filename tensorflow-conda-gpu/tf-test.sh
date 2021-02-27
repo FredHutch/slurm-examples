2365058f11abfb3b19056fe0b564985e01b5885a
Michael Gutteridge <michael.gutteridge@gmail.com>
2021-02-26 16:03:03 -0800

    Remove distracting line

--- tensorflow-conda-gpu/tf-test.sh
+++ tensorflow-conda-gpu/tf-test.sh
@@ -4,7 +4,6 @@ set -xe
 
 module load Anaconda3
 
-# the following line is likely temporary and may not be needed.
 . /app/software/Anaconda3/2020.02/etc/profile.d/conda.sh
 
 conda activate tfconda
