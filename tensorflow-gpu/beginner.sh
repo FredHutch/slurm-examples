0957099151d86f44770ba8de4004991fac9c945a
Michael Gutteridge <michael.gutteridge@gmail.com>
2023-12-06 11:34:45 -0800

    Initial commit of updated example

---
+++ tensorflow-gpu/beginner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+
+set -e
+
+MODULE="TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0"
+me=$(basename $0)
+rootpath=$(dirname $0)
+
+echo "${me}: Running beginner example with ${MODULE}"
+ml ${MODULE}
+
+python ${rootpath}/beginner.py
+echo "${me}: Script exited with code $?"
